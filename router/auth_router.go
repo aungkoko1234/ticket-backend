011fdac767e22378f934ade8ab15c256b6f70ecf
Aung Ko Ko <aung.koko@nfq.com>
2023-10-30 16:13:44 +0700

    reafractor : route folders

---
+++ router/auth_router.go
@@ -0,0 +1,23 @@
+package router
+
+import (
+	"github.com/aungkoko1234/tickermaster_backend/controller"
+	repository "github.com/aungkoko1234/tickermaster_backend/repository/user"
+	service "github.com/aungkoko1234/tickermaster_backend/service/user"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"gorm.io/gorm"
+)
+
+func IntiAuthRouters(db *gorm.DB, route *gin.RouterGroup,validate * validator.Validate ) {
+	 	//init repository
+	usersRepository := repository.NewUsersRepositoryImpl(db)
+
+	//init service
+	usersService := service.NewUsersServiceImpl(usersRepository, validate)
+
+	authController := controller.NewAuthController(usersService)
+
+	authRouter := route.Group("/auth")
+	authRouter.POST("login",authController.Login)
+}
---
+++ router/user_router.go
@@ -0,0 +1,34 @@
+package router
+
+import (
+	"github.com/aungkoko1234/tickermaster_backend/controller"
+	"github.com/aungkoko1234/tickermaster_backend/middleware"
+	repository "github.com/aungkoko1234/tickermaster_backend/repository/user"
+	service "github.com/aungkoko1234/tickermaster_backend/service/user"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"gorm.io/gorm"
+)
+
+func InitUserRouters(db *gorm.DB, route *gin.RouterGroup,validate * validator.Validate ) {
+		 	//init repository
+			 usersRepository := repository.NewUsersRepositoryImpl(db)
+
+			 //init service
+			 usersService := service.NewUsersServiceImpl(usersRepository, validate)
+
+			 userController:= controller.NewUserController(usersService)
+
+			 userRouter := route.Group("/users")
+			 userRouter.Use(middleware.JwtAuthMiddleware())
+
+		 
+		 
+			 userRouter.GET("",userController.FindAll)
+			 userRouter.GET("/:userId", userController.FindById)
+			 userRouter.POST("", userController.Create)
+			 userRouter.PATCH("/:userId", userController.Update)
+			 userRouter.DELETE("/:userId", userController.Delete)
+		 
+
+}
